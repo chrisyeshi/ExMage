fad19652b58750aad59406905a866747760e76fd
Chris Ye <chrisyeshi@gmail.com>
2013-04-15 19:17:36 -0700

    + put make path utitlity function to external file

--- Simulator.cpp
+++ Simulator.cpp
@@ -15,6 +15,7 @@
 #include "Frame.h"
 #include "ConfigReader.h"
 #include "ProcIndex.h"
+#include "mkpath.h"
 
 extern "C" {
   int readData(const char* filename,
@@ -25,70 +26,6 @@ extern "C" {
 
 #define TAG_TIMESTEP 1
 #define TAG_TIMESTEP_DATA 2
-
-///////////////////////////////////////////////////////////////////////////////////
-//
-//
-//
-// Make Directory
-//
-//
-//
-///////////////////////////////////////////////////////////////////////////////////
-
-typedef struct stat Stat;
-
-static int do_mkdir(const char *path, mode_t mode)
-{
-    Stat            st;
-    int             status = 0;
-
-    if (stat(path, &st) != 0)
-    {
-        /* Directory does not exist */
-        if (mkdir(path, mode) != 0)
-            status = -1;
-    }
-    else if (!S_ISDIR(st.st_mode))
-    {
-        errno = ENOTDIR;
-        status = -1;
-    }
-
-    return(status);
-}
-
-/**
- * ** mkpath - ensure all directories in path exist
- * ** Algorithm takes the pessimistic view and works top-down to ensure
- * ** each directory in path exists, rather than optimistically creating
- * ** the last element and working backwards.
- * */
-int mkpath(const char *path, mode_t mode)
-{
-    char           *pp;
-    char           *sp;
-    int             status;
-    char           *copypath = strdup(path);
-
-    status = 0;
-    pp = copypath;
-    while (status == 0 && (sp = strchr(pp, '/')) != 0)
-    {
-        if (sp != pp)
-        {
-            /* Neither root nor double slash in path */
-            *sp = '\0';
-            status = do_mkdir(copypath, mode);
-            *sp = '/';
-        }
-        pp = sp + 1;
-    }
-    if (status == 0)
-        status = do_mkdir(path, mode);
-    free(copypath);
-    return (status);
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 //
