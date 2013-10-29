5aa0d61f63d02c0830c60ee319062b6abd7b70ea
Chris Ye <chrisyeshi@gmail.com>
2013-10-28 20:33:59 -0700

    Changed the particle advector to use the new particle class, next is to change the tubing to use the new particle class too

--- main.cpp
+++ main.cpp
@@ -15,8 +15,7 @@
 #include "mkpath.h"
 
 CoreTube coretube;
-std::vector<tube::Particle> translate2tubeparticle(const std::vector<Particle>& particles);
-void output();
+std::vector<tube::Particle> translate2tubeparticle(const std::vector<Particle<> >& particles);
 
 int main(int argc, char* argv[])
 {
@@ -73,75 +72,16 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-std::vector<tube::Particle> translate2tubeparticle(const std::vector<Particle>& particles)
+std::vector<tube::Particle> translate2tubeparticle(const std::vector<Particle<> >& particles)
 {
     std::vector<tube::Particle> ret(particles.size());
     for (unsigned int i = 0; i < particles.size(); ++i)
     {
-        ret[i].x = particles[i].position[0];
-        ret[i].y = particles[i].position[1];
-        ret[i].z = particles[i].position[2];
-        ret[i].pd = particles[i].scalars[0];
-        ret[i].id = particles[i].tube_id;
+        ret[i].x = particles[i].coord()[0];
+        ret[i].y = particles[i].coord()[1];
+        ret[i].z = particles[i].coord()[2];
+        ret[i].pd = particles[i].scalar(0);
+        ret[i].id = particles[i].id();
     }
     return ret;
 }
-/*
-void output()
-{
-    ConfigReader& config = ConfigReader::getInstance();
-    // output frames
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::cout << "Proc: " << rank << " Progress: Saving..." << std::endl;
-    for (int i = 0; i < coretube.GetCameraCount(); ++i)
-    {
-        Frame* sum = coretube.GetFrame(i);
-        char proc_index_string[10];
-        sprintf(proc_index_string, "%02d", rank);
-        char cam_index_string[10];
-        sprintf(cam_index_string, "%02d", i);
-
-        char pcs[100];
-        int resolution[2];
-        sum->GetSize(resolution);
-        sprintf(pcs, "n%d_p%d_r%d_t%d",
-                config.GetRegionCount()[0],
-                config.GetRegionParticleCount(),
-                resolution[0],
-                config.GetTimeStepRange()[1] - config.GetTimeStepRange()[0]);
-
-        std::string dir = config.GetOutRoot() + "/" + pcs
-                        + std::string("/cam") + cam_index_string;
-        std::string spt_path = dir + "/output_proc" + proc_index_string;
-        mkpath(dir.c_str(), 0777);
-        mkpath(dir.c_str(), 0777);
-        mkpath(dir.c_str(), 0777);
-        sum->SetFileName(spt_path.c_str());
-        sum->Write();
-    }
-    // output times
-    Frame* sum = coretube.GetFrame(0);
-    char pcs[100];
-    int resolution[2];
-    sum->GetSize(resolution);
-    sprintf(pcs, "n%d_p%d_r%d_t%d",
-            config.GetRegionCount()[0],
-            config.GetRegionParticleCount(),
-            resolution[0],
-            config.GetTimeStepRange()[1] - config.GetTimeStepRange()[0]);
-
-    std::string dir = config.GetOutRoot() + "/" + pcs + "/time";
-    char global_index_string[10];
-    sprintf(global_index_string, "%02d", rank);
-    std::string time_path = dir + "/proc" + global_index_string + ".txt";
-    mkpath(dir.c_str(), 0777);
-    mkpath(dir.c_str(), 0777);
-    mkpath(dir.c_str(), 0777);
-    std::ofstream tout(time_path.c_str());
-    for (unsigned int i = 0; i < times.size(); ++i)
-    {
-        tout << times[i] << ",";
-    }
-}
-*/
--- utilities/Particle.h
+++ utilities/Particle.h
@@ -1,6 +1,8 @@
 #ifndef __Particle_h__
 #define __Particle_h__
 
+#include <vector>
+
 template <class T = float>
 class Particle
 {
@@ -24,8 +26,10 @@ public:
 	std::vector<T>& coord() { return Coord; }
 	const std::vector<T>& scalars() const { return Scalars; }
 	std::vector<T>& scalars() { return Scalars; }
-	T scalar(unsigned idx) const { return Scalars[idx]; }
-	T& scalar(unsigned idx) { return Scalars[idx]; }
+	T scalar(unsigned idx) const;
+	T& scalar(unsigned idx);
+	unsigned int id() const { return Id; }
+	unsigned int& id() { return Id; }
 
 protected:
 	unsigned int Id;
@@ -34,5 +38,53 @@ protected:
 
 private:
 };
+
+////////////////////////////////////////////////////
+//
+//
+//
+//
+// Implementations
+//
+//
+//
+//
+////////////////////////////////////////////////////
+template <class T>
+Particle<T>::Particle() : Coord(3)
+{
+	Coord[0] = Coord[1] = Coord[2] = 0.0;
+}
+
+template <class T>
+T Particle<T>::operator[](unsigned int idx) const
+{
+	if (idx < 3)
+		return Coord[idx];
+	return Scalars[idx - 3];
+}
+
+template <class T>
+T& Particle<T>::operator[](unsigned int idx)
+{
+	if (idx < 3)
+		return Coord[idx];
+	return Scalars[idx - 3];
+}
+
+template <class T>
+T Particle<T>::scalar(unsigned int idx) const
+{
+	assert(idx < Scalars.size());
+	return Scalars[idx];
+}
+
+template <class T>
+T& Particle<T>::scalar(unsigned int idx)
+{
+	if (idx >= Scalars.size())
+		Scalars.resize(idx + 1);
+	return Scalars[idx];
+}
 
 #endif //__Particle_h__
