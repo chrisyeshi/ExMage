8ab1d9016d7c5074a8a0fdbb08d6edc5f96260dc
Chris Ye <chrisyeshi@gmail.com>
2013-10-29 17:19:50 -0700

    Added test for new Vector class, only for 3d case though. Next will be substituting the old Vector class with the new one.

---
+++ test/utilities/vector3d.cpp
@@ -0,0 +1,63 @@
+#include "Vector.h"
+
+#include <cassert>
+#include <iostream>
+
+int main(void)
+{
+	// accessors
+	Vector<3> v;
+	v[0] = 1.f;
+	v[1] = 1.f;
+	v[2] = 1.f;
+	std::cout << "testing the accessors..." << std::endl;
+	assert(fabs(v.x() - 1.f) < 0.0001);
+	assert(fabs(v.y() - 1.f) < 0.0001);
+	assert(fabs(v.z() - 1.f) < 0.0001);
+	std::cout << "accessors working!" << std::endl;
+
+	Vector<3> v1, v2, vr, vc;
+	v1[0] = v1[1] = v1[2] = v2[0] = v2[1] = v2[2] = 1.f;
+	// operator==
+	assert(v1 == v2);
+	// operator!=
+	vr = v1 + v2;
+	assert(v1 != vr);
+	// operator+
+	vr = v1 + 1.f;
+	assert(vr == Vector<3>(2.f, 2.f, 2.f));
+	vr = v1 + v2;
+	assert(vr == Vector<3>(2.f, 2.f, 2.f));
+	// operator-
+	vr = v1 - 1.f;
+	assert(vr == Vector<3>(0.f, 0.f, 0.f));
+	vr = v1 - v2;
+	assert(vr == Vector<3>(0.f, 0.f, 0.f));
+	// operator*
+	vr = v1 * 1.f;
+	assert(vr == Vector<3>(1.f, 1.f, 1.f));
+	vr = v1 * v2;
+	assert(vr == Vector<3>(1.f, 1.f, 1.f));
+	// operator/
+	vr = v1 / 1.f;
+	assert(vr == Vector<3>(1.f, 1.f, 1.f));
+	vr = v1 / v2;
+	assert(vr == Vector<3>(1.f, 1.f, 1.f));
+	// dot
+	float d = dot(v1, v2);
+	assert(fabs(d - 3.f) < 0.0001);
+	// cross
+	vr = cross(Vector<3>(0.f, 1.f, 0.f), Vector<3>(1.f, 0.f, 0.f));
+	assert(vr == Vector<3>(0.f, 0.f, -1.f));
+	// length
+	float l = vr.length();
+	assert(fabs(l - 1.f) < 0.0001);
+	// normal
+	Vector<3> n = vr.normal();
+	vr.normalize();
+	assert(n == vr);
+	assert(n == Vector<3>(0.f, 0.f, -1.f));
+	std::cout << "all working!" << std::endl;
+
+	return 0;
+}
--- utilities/Vector.h
+++ utilities/Vector.h
@@ -2,19 +2,12 @@
 #include <cmath>
 
 template <int Dim = 3, class T = float>
-class Vector;
-
-template <int Dim = 3, class T = float>
-T dot(const Vector<Dim, T>& l, const Vector<Dim, T>& r);
-
-template <int Dim = 3, class T = float>
-T cross(const Vector<Dim, T>& l, const Vector<Dim, T>& r);
-
-template <int Dim = 3, class T = float>
 class Vector
 {
 public:
 	Vector();
+	Vector(const Vector<Dim, T>& v);
+	Vector(const T& x, const T& y = 0.0, const T& z = 0.0);
 
 	// convenient accessors
 	T operator[](unsigned int idx) const;
@@ -29,6 +22,9 @@ public:
 	T& w() { return Vals[4]; }
 
 	// operators
+	// ==
+	bool operator==(const Vector<Dim, T>& right) const;
+	bool operator!=(const Vector<Dim, T>& right) const;
 	// +
 	Vector<Dim, T> operator+(const T& right) const;
 	Vector<Dim, T> operator+(const Vector<Dim, T>& right) const;
@@ -41,15 +37,14 @@ public:
 	// /
 	Vector<Dim, T> operator/(const T& right) const;
 	Vector<Dim, T> operator/(const Vector<Dim, T>& right) const;
-	// dot product
-	friend T dot(const Vector<Dim, T>& l, const Vector<Dim, T>& r);
-	// cross product
-	friend Vector<Dim, T> cross(const Vector<Dim, T>& l, const Vector<Dim, T>& r);
 	// length
 	T length() const;
 	// normalize
 	Vector<Dim, T> normal() const;
 	void normalize();
+	//
+	// dot product and cross product are provided as external functions in this file.
+	//
 
 protected:
 
@@ -64,6 +59,23 @@ Vector<Dim, T>::Vector()
 }
 
 template <int Dim, class T>
+Vector<Dim, T>::Vector(const Vector<Dim, T>& v)
+{
+	for (int i = 0; i < Dim; ++i)
+		this->Vals[i] = v[i];
+}
+
+template <int Dim, class T>
+Vector<Dim, T>::Vector(const T& x, const T& y, const T& z)
+{
+	this->Vals[0] = x;
+	if (Dim <= 1) return;
+	this->Vals[1] = y;
+	if (Dim <= 2) return;
+	this->Vals[2] = z;
+}
+
+template <int Dim, class T>
 T Vector<Dim, T>::operator[](unsigned int idx) const
 {
 	assert(idx < Dim);
@@ -75,6 +87,21 @@ T& Vector<Dim, T>::operator[](unsigned int idx)
 {
 	assert(idx < Dim);
 	return Vals[idx];
+}
+
+template <int Dim, class T>
+bool Vector<Dim, T>::operator==(const Vector<Dim, T>& right) const
+{
+	for (int i = 0; i < Dim; ++i)
+		if (fabs(this->Vals[i] - right[i]) >= 0.0001)
+			return false;
+	return true;
+}
+
+template <int Dim, class T>
+bool Vector<Dim, T>::operator!=(const Vector<Dim, T>& right) const
+{
+	return !(*this == right);
 }
 
 template <int Dim, class T>
@@ -163,14 +190,14 @@ Vector<Dim, T> cross(const Vector<Dim, T>& l, const Vector<Dim, T>& r)
 {
 	assert(Dim == 3);
 	Vector<Dim, T> ret;
-	ret[0] = l[1]r[2] - l[2]r[1];
-	ret[1] = l[2]r[0] - l[0]r[2];
-	ret[2] = l[0]r[1] - l[1]r[0];
+	ret[0] = l[1] * r[2] - l[2] * r[1];
+	ret[1] = l[2] * r[0] - l[0] * r[2];
+	ret[2] = l[0] * r[1] - l[1] * r[0];
 	return ret;
 }
 
 template <int Dim, class T>
-T length() const
+T Vector<Dim, T>::length() const
 {
 	T sqsum = 0.0;
 	for (int i = 0; i < Dim; ++i)
@@ -179,13 +206,13 @@ T length() const
 }
 
 template <int Dim, class T>
-Vector<Dim, T> normal() const
+Vector<Dim, T> Vector<Dim, T>::normal() const
 {
 	return *this / this->length();
 }
 
 template <int Dim, class T>
-void normalize()
+void Vector<Dim, T>::normalize()
 {
 	T len = this->length();
 	for (int i = 0; i < Dim; ++i)
